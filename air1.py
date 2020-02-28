6bb1c2c36dcbd1d63e6b5e9dee80b46705361e23
Salman Karim <44101710+salmansust@users.noreply.github.com>
2020-02-28 22:04:35 +0600

    Add files via upload

---
+++ air1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 09:04:22 2020
+
+@author: SalmanKarim
+"""
+
+# load data and split into chunks
+from numpy import unique
+from pandas import read_csv
+
+# split the dataset by 'chunkID', return a dict of id to rows
+def to_chunks(values, chunk_ix=1):
+	chunks = dict()
+	# get the unique chunk ids
+	chunk_ids = unique(values[:, chunk_ix])
+	# group rows by chunk id
+	for chunk_id in chunk_ids:
+		selection = values[:, chunk_ix] == chunk_id
+		chunks[chunk_id] = values[selection, :]
+	return chunks
+
+# load dataset
+dataset = read_csv('TrainingData.csv', header=0)
+# group data by chunks
+values = dataset.values
+chunks = to_chunks(values)
+print('Total Chunks: %d' % len(chunks))
